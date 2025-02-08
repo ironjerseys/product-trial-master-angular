cfb044ea93ff2ec8eefd8bb82f767a5462186161
Joris Reynes <jo.r6349@gmail.com>
2025-02-08 16:20:15 +0100

    api call and product file deleted

--- src/app/products/data-access/products.service.ts
+++ src/app/products/data-access/products.service.ts
@@ -4,51 +4,58 @@ import { HttpClient } from "@angular/common/http";
 import { catchError, Observable, of, tap } from "rxjs";
 
 @Injectable({
-    providedIn: "root"
-}) export class ProductsService {
+  providedIn: "root",
+})
+export class ProductsService {
+  private readonly http = inject(HttpClient);
+  private readonly path = "/api/products";
+  private apiUrl = "https://localhost:7280/api/Products";
 
-    private readonly http = inject(HttpClient);
-    private readonly path = "/api/products";
-    
-    private readonly _products = signal<Product[]>([]);
+  private readonly _products = signal<Product[]>([]);
 
-    public readonly products = this._products.asReadonly();
+  public readonly products = this._products.asReadonly();
 
-    public get(): Observable<Product[]> {
-        return this.http.get<Product[]>(this.path).pipe(
-            catchError((error) => {
-                return this.http.get<Product[]>("assets/products.json");
-            }),
-            tap((products) => this._products.set(products)),
-        );
-    }
+  public get(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.path).pipe(
+      catchError((error) => {
+        return this.http.get<Product[]>(this.apiUrl);
+      }),
+      tap((products) => this._products.set(products))
+    );
+  }
 
-    public create(product: Product): Observable<boolean> {
-        return this.http.post<boolean>(this.path, product).pipe(
-            catchError(() => {
-                return of(true);
-            }),
-            tap(() => this._products.update(products => [product, ...products])),
-        );
-    }
+  public create(product: Product): Observable<boolean> {
+    return this.http.post<boolean>(this.path, product).pipe(
+      catchError(() => {
+        return of(true);
+      }),
+      tap(() => this._products.update((products) => [product, ...products]))
+    );
+  }
 
-    public update(product: Product): Observable<boolean> {
-        return this.http.patch<boolean>(`${this.path}/${product.id}`, product).pipe(
-            catchError(() => {
-                return of(true);
-            }),
-            tap(() => this._products.update(products => {
-                return products.map(p => p.id === product.id ? product : p)
-            })),
-        );
-    }
+  public update(product: Product): Observable<boolean> {
+    return this.http.patch<boolean>(`${this.path}/${product.id}`, product).pipe(
+      catchError(() => {
+        return of(true);
+      }),
+      tap(() =>
+        this._products.update((products) => {
+          return products.map((p) => (p.id === product.id ? product : p));
+        })
+      )
+    );
+  }
 
-    public delete(productId: number): Observable<boolean> {
-        return this.http.delete<boolean>(`${this.path}/${productId}`).pipe(
-            catchError(() => {
-                return of(true);
-            }),
-            tap(() => this._products.update(products => products.filter(product => product.id !== productId))),
-        );
-    }
+  public delete(productId: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.path}/${productId}`).pipe(
+      catchError(() => {
+        return of(true);
+      }),
+      tap(() =>
+        this._products.update((products) =>
+          products.filter((product) => product.id !== productId)
+        )
+      )
+    );
+  }
 }
